45159757ec260b66eadd6aadeb28badd1678cf48
John Doe <johndoe@example.com>
2016-03-24 16:31:36 +0800

    xx

---
+++ MyCms/src/com/xt/service/task/TestTask.java
@@ -0,0 +1,10 @@
+package com.xt.service.task;
+
+//@Component("TestTask")
+public class TestTask {
+//	@Scheduled(cron = "1/1 * * * * ?")
+//	public void job(){
+//		System.out.println("对应的任务正在执行");
+//	}
+
+}
